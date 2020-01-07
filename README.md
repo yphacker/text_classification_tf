5f3cfcaee31a5e0145fc98176c70385431255f63
yphacker <yphacker@163.com>
2020-01-07 23:57:58 +0800

    optimize

--- README.md
+++ README.md
@@ -5,14 +5,20 @@
 ## 评估标准
 roc_auc
 ## kaggle score:
-ml:0.95188
-数据扩充+ml:0.96977
-cnn:0.94521
-bilstm:0.84620
-bilstm+attention:0.86084
-rcnn:0.85112
-transformer:0.86324
-bert:0.96029
+|model|score|
+|---|---|
+|ml|0.95188|
+|数据扩充+ml|0.96977|
+|cnn|0.94521|
+|bilstm|0.84620|
+|bilstm+attention|0.86084|
+|rcnn|0.85112|
+|transformer|0.86324|
+|bert|0.96029|
+|albert(base_v2)|0.91513|
 
 ## 参考链接
-[brightmart/text_classification](https://github.com/brightmart/text_classification)
+[google-research/bert](https://github.com/google-research/bert)
+[google-research/ALBERT](https://github.com/google-research/ALBERT)
+
+
--- src/conf/config.py
+++ src/conf/config.py
@@ -8,15 +8,14 @@ conf_path = os.path.dirname(os.path.abspath(__file__))
 work_path = os.path.dirname(os.path.dirname(conf_path))
 data_path = os.path.join(work_path, "data")
 
-origin_data_path = os.path.join(data_path, "origin_data")
-origin_train_path = os.path.join(origin_data_path, 'labeledTrainData.tsv')
-origin_test_path = os.path.join(origin_data_path, 'testData.tsv')
-sample_submission_path = os.path.join(origin_data_path, 'sampleSubmission.csv')
+train_path = os.path.join(data_path, 'labeledTrainData.tsv')
+test_path = os.path.join(data_path, 'testData.tsv')
+sample_submission_path = os.path.join(data_path, 'sampleSubmission.csv')
 
-process_data_path = os.path.join(data_path, "process_data")
-text_path = os.path.join(process_data_path, 'text.txt')
-train_path = os.path.join(process_data_path, 'train.csv')
-test_path = os.path.join(process_data_path, 'test.csv')
+# process_data_path = os.path.join(data_path, "process_data")
+# text_path = os.path.join(process_data_path, 'text.txt')
+# train_path = os.path.join(process_data_path, 'train.csv')
+# test_path = os.path.join(process_data_path, 'test.csv')
 
 stop_words_path = os.path.join(data_path, 'english')
 word2vec_path = os.path.join(data_path, 'word2vec.bin')
@@ -25,18 +24,14 @@ word2id_json_path = os.path.join(data_path, 'word2id.json')
 label2id_json_path = os.path.join(data_path, 'label2id.json')
 
 train_check_path = os.path.join(data_path, 'train_check.txt')
-model_submission_path = os.path.join(data_path, 'submission.csv')
 
 model_path = os.path.join(data_path, 'model')
 
-# vocab_size = 5000  # 词汇表大小
 vocab_size = 28607  # 词汇表达大小
 embedding_size = 200
-# embedding_size = 64
 max_seq_len = 200  # 序列长度
 num_labels = 2  # 类别数量
 epochs_num = 8
 batch_size = 32
-# save_per_batch = 10
 print_per_batch = 10
 improvement_step = print_per_batch * 10
